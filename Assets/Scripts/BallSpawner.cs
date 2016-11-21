8087b4c495bc0ae07c835bcdee08cdf868879f14
TAKAKO OHSHIMA <hatone@TAKAKOs-MacBook-Pro.local>
2016-11-20 23:03:58 -0800

    Add Camera Gimbal

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject instance = Instantiate (ballPrefab);
+			Rigidbody rb = instance.GetComponent<Rigidbody> ();
+			rb.velocity = Vector3.left;
+		}
+	}
+}
---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationSpeed = 1.0f;
+		float mouseX = Input.GetAxis ("Mouse X") * rotationSpeed;
+		float mouseY = Input.GetAxis ("Mouse Y") * rotationSpeed;
+		Camera camera = GetComponentInChildren<Camera> ();
+		transform.localRotation = Quaternion.Euler (0, mouseX, 0) * transform.localRotation;
+		camera.transform.localRotation = Quaternion.Euler (-mouseY, 0, 0) * camera.transform.localRotation;
+	}
+}
